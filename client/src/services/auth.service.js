133249283026475e0ca13430136afbb43f0c2b04
lunarss <woshiyinhao@gmail.com>
2023-01-07 18:04:09 -0500

    add dotenv to client

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/user";
+const API_URL = "http://" + process.env.REACT_APP_LOCALHOST + ":8080/api/user";
 
 class AuthService {
   login(email, password) {
--- client/src/services/task.service.js
+++ client/src/services/task.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/tasks";
+const API_URL = "http://" + process.env.REACT_APP_LOCALHOST + ":8080/api/tasks";
 
 class TaskService {
   // create a new task
